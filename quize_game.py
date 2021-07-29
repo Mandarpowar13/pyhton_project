15302c7e94287651520ecb4857f44bbce9e04443
mandarpowar13 <mandarpowar328@gmail.com>
2021-07-29 19:50:01 +0530

    update_quizze-game

--- quize_game.py
+++ quize_game.py
@@ -7,27 +7,61 @@ if playing.lower() !="yes":
 print("okay lets play!!")
 score=0
 answer=input("what dose CPU stands for? ")
-if answer.lower() == "central processing unit":
-    print("correct!")
-    score +=1
-else:
-    print("incorrect!")
+guess_count=0
+guess_limit=3
+out_of_guess=False
+while answer.lower() != "central processing unit" and not(out_of_guess):
+      if guess_count < guess_limit:
+        answer = input("Your answer is wrong!you got another guess : ")
+        guess_count += 1
+      else:
+        out_of_guess = True
 
-answer=input("which city is capital of India? ")
-if answer.lower() == "delhi":
-    print("correct!")
-    score +=1
-else:
-    print("incorrect!")
 
-answer=input("which is popular programming launguge? ")
-if answer.lower() == "python":
-    print("correct!")
-    score +=1
+if out_of_guess:
+    print("You are out of guess! Next question")
 else:
-    print("incorrect!")
+    print("Correct!")
+    score +=1  
 
-print("you got "+ str(score)+" answer correct!!")
+answer1=input("which city is capital of India? ")
+guess_count=0
+guess_limit=3
+out_of_guess=False
+while answer1.lower() != "delhi" and not (out_of_guess):
+        if guess_count < guess_limit:
+         answer1 = input("Your answer is WRONG!! You got another guess : ")
+         guess_count += 1
+        else:
+         out_of_guess = True
+
+
+if out_of_guess:
+    print("You are out of guess!! Next question")
+else:
+    print("Correct!")
+    score += 1
+
+
+answer2=input("which is popular programming launguge? ")
+guess_count=0
+guess_limit=3
+out_of_guess=False
+while answer2.lower() != "python" and not (out_of_guess):
+    if guess_count < guess_limit:
+         answer1 = input("Your answer is wrong!you got another guess : ")
+         guess_count += 1
+    else:
+         out_of_guess = True
+
+
+if out_of_guess:
+    print("You are out of guess! Next question")
+else:
+    print("Correct!")
+    score += 1
+
+print("YOU GOT "+ str(score)+" ANSWER CORRECT!!")
 
   
 
